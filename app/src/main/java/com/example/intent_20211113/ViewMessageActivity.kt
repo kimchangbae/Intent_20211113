2d653e7efc1c39b949b755346f8f37cdab4cce55
김창배 <cbkim93@naver.com>
2021-11-13 15:47:01 +0900

    Intent(2) - 받아서 활용하는 화면  - intent.getStringExtra 활용

--- app/src/main/java/com/example/intent_20211113/ViewMessageActivity.kt
+++ app/src/main/java/com/example/intent_20211113/ViewMessageActivity.kt
@@ -1,12 +1,22 @@
 package com.example.intent_20211113
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message)
 
+        val sendMsg = intent.getStringExtra("msg")
+        txtMessage.text = sendMsg
+
+        btnBack.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
