54737d65819ca13368f5c6f0ebea23754a1d310e
김창배 <cbkim93@naver.com>
2021-11-13 15:40:48 +0900

    Intent(2) - 보내는 화면에 데이터 담아서 이동

--- app/src/main/java/com/example/intent_20211113/MainActivity.kt
+++ app/src/main/java/com/example/intent_20211113/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.intent_20211113
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +14,21 @@ class MainActivity : AppCompatActivity() {
         btnMoveToOther.setOnClickListener {
             val myIntent = Intent(this, SubActivity::class.java)
             startActivity(myIntent)
-            finish()
+        }
+
+        btnSendMessage.setOnClickListener {
+            // ViewMessageActivity 화면으로 이동하기 + 입력한 문구도 가지고 이동하기
+            // 1. 입력한 내용을 기록해두자. (변수에 담아두자)
+
+            val inputMessage = edtMessage.text.toString()
+
+            // 2. 화면 이동
+            val myIntent = Intent(this, ViewMessageActivity::class.java)
+            myIntent.putExtra("msg",inputMessage)
+            startActivity(myIntent)
+
         }
     }
+
+
 }
